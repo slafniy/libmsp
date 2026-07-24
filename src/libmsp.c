53ccc757018dc62fbd7780dec0a1d24f2cf37da7
slafniy <slafniy@gmail.com>
2026-07-24 21:55:17 +0200

    added deferred CLEAN macro

--- src/libmsp.c
+++ src/libmsp.c
@@ -7,12 +7,37 @@
 
 #include "libavformat/avformat.h"
 #include "libavutil/error.h"
+#include "libavcodec/avcodec.h"
+
+#define CLEAN(clean_func) __attribute__((cleanup(clean_func)))
 
 static pthread_t playback_thread;
 
 typedef struct {
     char *filename;
 } playback_args_t;
+
+// ReSharper disable once CppDeclaratorNeverUsed - uses via macro
+static void msp_free_playback_args(playback_args_t **playback_args) {
+    free((*playback_args)->filename);
+    (*playback_args)->filename = nullptr;
+    free(*playback_args);
+    *playback_args = nullptr;
+}
+
+typedef struct {
+    AVFormatContext *av_format_context;
+    AVCodecContext *av_codec_context;
+} playback_context_t;
+
+static void msp_free_playback_context(playback_context_t **playback_context) {
+    if (!playback_context || !*playback_context) return;
+    const auto ctx_ptr = *playback_context;
+    if (ctx_ptr->av_format_context) avformat_close_input(&ctx_ptr->av_format_context); // also sets it to NULL
+    if (ctx_ptr->av_codec_context) avcodec_free_context(&ctx_ptr->av_codec_context); // also sets it to NULL
+    free(ctx_ptr);
+    *playback_context = nullptr;
+}
 
 void msp_handle_ffmpeg_error(const char *what, int err) {
     char buf[AV_ERROR_MAX_STRING_SIZE];
@@ -22,30 +47,26 @@ void msp_handle_ffmpeg_error(const char *what, int err) {
 }
 
 void *playback_thread_func(void *arg) {
-    playback_args_t *args = arg;
-    printf("Playing: %s\n", args->filename);
+    CLEAN(msp_free_playback_args) playback_args_t *playback_args = arg;
 
-    AVFormatContext *fmt_ctx = nullptr;
-    int ret;
+    // ReSharper disable once CppDFAMemoryLeak
+    CLEAN(msp_free_playback_context) playback_context_t *playback_context = malloc(sizeof(playback_context_t));
 
-    ret = avformat_open_input(&fmt_ctx, args->filename, nullptr, nullptr);
+    printf("Playing: %s\n", playback_args->filename);
+
+    int ret = avformat_open_input(&playback_context->av_format_context, playback_args->filename, nullptr, nullptr);
     if (ret < 0) {
         msp_handle_ffmpeg_error("avformat_open_input", ret);
-        goto error;
+        return nullptr;
     }
 
-    ret = avformat_find_stream_info(fmt_ctx, nullptr);
+    ret = avformat_find_stream_info(playback_context->av_format_context, nullptr);
     if (ret < 0) {
         msp_handle_ffmpeg_error("avformat_find_stream_info", ret);
-        goto error;
+        return nullptr;
     }
 
-    printf("Container: %s\n", fmt_ctx->iformat->name);
-
-error:
-    avformat_close_input(&fmt_ctx);
-    free(args->filename);
-    free(args);
+    printf("Container: %s\n", playback_context->av_format_context->iformat->name);
 
     return nullptr;
 }
@@ -60,18 +81,15 @@ int msp_play(const char *filename) {
 
     args->filename = strdup(filename);
     if (!args->filename) {
-        goto error;
+        // ReSharper disable once CppDFAMemoryLeak
+        return -1;
     }
 
     if (pthread_create(&playback_thread, nullptr, playback_thread_func, args) != 0) {
-        goto error;
+        // ReSharper disable once CppDFAMemoryLeak
+        return -1;
     }
 
     // ReSharper disable once CppDFAMemoryLeak
     return 0;
-
-error:
-    free(args->filename);
-    free(args);
-    return -1;
 }
