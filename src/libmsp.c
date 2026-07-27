080b24ace748798299d0345a85bcbe7c4dabe9a2
slafniy <slafniy@gmail.com>
2026-07-27 06:14:36 +0200

    added a huge bunch of the code to find a correct decoder

--- src/libmsp.c
+++ src/libmsp.c
@@ -10,6 +10,7 @@
 #include "libavformat/avformat.h"
 #include "libavutil/error.h"
 #include "libavcodec/avcodec.h"
+#include "libswresample/swresample.h"
 #include "SDL2/SDL.h"
 
 #define DEFERRED_CLEANUP(clean_func) __attribute__((cleanup(clean_func)))
@@ -25,6 +26,19 @@ fprintf(stderr, "[%s]: " fmt "\n", (who) __VA_OPT__(,) __VA_ARGS__); \
 #define PLAYBACK_THREAD_NAME "libmsp playback"
 #define MAIN_THREAD_NAME "libmsp main"
 
+#define SAMPLE_RATE 96000
+
+const SDL_AudioSpec msp_sdl_wanted_spec = {
+    .freq = SAMPLE_RATE,
+    .format = AUDIO_F32,
+    .channels = 2,
+    .samples = 2048,
+    .callback = nullptr
+};
+
+// should match msp_sdl_wanted_spec!
+const AVChannelLayout msp_av_channel_layout = AV_CHANNEL_LAYOUT_STEREO;
+constexpr auto msp_av_sample_format = AV_SAMPLE_FMT_FLT; // this is for float in sdl2
 
 typedef struct {
     // libmsp specific data
@@ -32,8 +46,12 @@ typedef struct {
     msp_status_t status; // playback status, used to control playback thread
 
     // ffmpeg data - open file, find stream, decode etc
+    int audio_stream_index;
     AVFormatContext *av_format_context;
     AVCodecContext *av_codec_context;
+    SwrContext *swr_context;
+    AVPacket *av_packet;
+    AVFrame *av_frame;
 } playback_context_t;
 
 //======================================================================================================================
@@ -43,13 +61,16 @@ static pthread_t msp_playback_thread; // handles commands and uses ffmpeg libs t
 static msp_command_q_t *msp_command_q; // queue for commands for playback thread
 static SDL_AudioDeviceID msp_sdl_device_id;
 
-// ReSharper disable once CppDeclaratorNeverUsed - uses via macro
+// ReSharper disable once CppDeclaratorNeverUsed - it is used via macro
 static void msp_free_playback_context(playback_context_t **playback_context) {
     if (!playback_context || !*playback_context) return;
     const auto ctx_ptr = *playback_context;
     if (ctx_ptr->filename) free(ctx_ptr->filename);
     if (ctx_ptr->av_format_context) avformat_close_input(&ctx_ptr->av_format_context); // also sets it to NULL
     if (ctx_ptr->av_codec_context) avcodec_free_context(&ctx_ptr->av_codec_context); // also sets it to NULL
+    if (ctx_ptr->swr_context) swr_free(&ctx_ptr->swr_context);
+    if (ctx_ptr->av_packet) av_packet_free(&ctx_ptr->av_packet);
+    if (ctx_ptr->av_frame) av_frame_free(&ctx_ptr->av_frame);
     free(ctx_ptr);
     *playback_context = nullptr;
 }
@@ -82,15 +103,9 @@ bool msp_init_sdl2() {
         LOG_ERROR(MAIN_THREAD_NAME, "Cannot init SDL: %s", SDL_GetError());
         return false;
     }
-    const SDL_AudioSpec wanted_spec = {
-        .freq = 96000,
-        .format = AUDIO_F32,
-        .channels = 2,
-        .samples = 2048,
-        .callback = nullptr
-    };
+
     SDL_AudioSpec obtained_spec;
-    msp_sdl_device_id = SDL_OpenAudioDevice(nullptr, 0, &wanted_spec, &obtained_spec, 0);
+    msp_sdl_device_id = SDL_OpenAudioDevice(nullptr, 0, &msp_sdl_wanted_spec, &obtained_spec, 0);
     if (msp_sdl_device_id == 0) {
         LOG_ERROR(MAIN_THREAD_NAME, "Failed to open audio device: %s", SDL_GetError());
         SDL_QuitSubSystem(SDL_INIT_AUDIO);
@@ -115,25 +130,87 @@ void msp_deinit_sdl2() {
     SDL_QuitSubSystem(SDL_INIT_AUDIO);
 }
 
-// Playback thread function
-bool msp_open_new_file(playback_context_t *playback_context) {
-    int ret = avformat_open_input(&playback_context->av_format_context, playback_context->filename, nullptr, nullptr);
+// Playback thread function. Opens file, looks for audio stream, codec etc.
+bool msp_open_new_file(playback_context_t *ctx) {
+    // Trying to open audio file
+    int ret = avformat_open_input(&ctx->av_format_context, ctx->filename, nullptr, nullptr);
     if (ret < 0) {
         msp_handle_ffmpeg_error("avformat_open_input", ret);
-        LOG_ERROR(PLAYBACK_THREAD_NAME, "ffmpeg cannot open %s", playback_context->filename);
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "ffmpeg cannot open %s", ctx->filename);
         return false;
     }
 
-    ret = avformat_find_stream_info(playback_context->av_format_context, nullptr);
+    // Looking for container format
+    ret = avformat_find_stream_info(ctx->av_format_context, nullptr);
     if (ret < 0) {
         msp_handle_ffmpeg_error("avformat_find_stream_info", ret);
-        LOG_ERROR(PLAYBACK_THREAD_NAME, "ffmpeg cannot find stream info in file %s", playback_context->filename);
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "ffmpeg cannot find stream info in file %s", ctx->filename);
         return false;
     }
 
-    LOG_INFO(PLAYBACK_THREAD_NAME,
-             "Container <%s> is found in %s", playback_context->av_format_context->iformat->name,
-             playback_context->filename);
+    LOG_INFO(PLAYBACK_THREAD_NAME, "Container <%s> is found in %s", ctx->av_format_context->iformat->name,
+             ctx->filename);
+
+    // Looking for audio stream and decoder
+    const AVCodec *codec = nullptr;
+    const int stream_idx = av_find_best_stream(
+        ctx->av_format_context,
+        AVMEDIA_TYPE_AUDIO,
+        -1,
+        -1,
+        &codec,
+        0
+    );
+
+    if (stream_idx < 0) {
+        msp_handle_ffmpeg_error("av_find_best_stream", stream_idx);
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "Cannot find audio stream or decoder for %s", ctx->filename);
+        return false;
+    }
+
+    ctx->audio_stream_index = stream_idx;
+    const AVCodecParameters *codec_parameters = ctx->av_format_context->streams[stream_idx]->codecpar;
+    LOG_INFO(PLAYBACK_THREAD_NAME, "Found audio stream #%d, codec: %s", stream_idx, codec->name);
+
+    ctx->av_codec_context = avcodec_alloc_context3(codec);
+    if (!ctx->av_codec_context) {
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "Failed to allocate codec context");
+        return false;
+    }
+
+    if (avcodec_parameters_to_context(ctx->av_codec_context, codec_parameters) < 0) {
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "Failed to copy codec params");
+        return false;
+    }
+
+    if (avcodec_open2(ctx->av_codec_context, codec, nullptr) < 0) {
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "Failed to open codec");
+        return false;
+    }
+
+    // Prepare resampler
+    ret = swr_alloc_set_opts2(
+        &ctx->swr_context,
+        &msp_av_channel_layout,
+        msp_av_sample_format,
+        SAMPLE_RATE,
+        &ctx->av_codec_context->ch_layout,
+        ctx->av_codec_context->sample_fmt,
+        ctx->av_codec_context->sample_rate,
+        0, nullptr
+    );
+    if (ret < 0 || swr_init(ctx->swr_context) < 0) {
+        LOG_ERROR(PLAYBACK_THREAD_NAME, "Failed to initialize resampler");
+        return false;
+    }
+
+    // Allocate packet and frame for future decoding
+    ctx->av_packet = av_packet_alloc();
+    ctx->av_frame = av_frame_alloc();
+
+    LOG_INFO(PLAYBACK_THREAD_NAME, "Successfully loaded container <%s> with codec [%s]",
+             ctx->av_format_context->iformat->name, codec->name);
+
     return true;
 }
 
